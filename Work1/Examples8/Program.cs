503e34d56095862287428ca377e77c38dc99712a
SkorikLL <skorik555web@gmail.com>
2022-11-19 16:00:45 +0700

    add Examples8

---
+++ Work1/Examples8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Задача 8: Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.");
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+while(number > 0){
+    number--;
+    if (((number % 2) == 0 ))
+    Console.Write($"{number}, ");
+}
