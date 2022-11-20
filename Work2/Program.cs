0eae377a739e9b83194b0f9f9119116f95e5fe4d
SkorikLL <skorik555web@gmail.com>
2022-11-20 15:17:04 +0700

    Add in Work2 zadacha 13, 15

--- Work2/Program.cs
+++ Work2/Program.cs
@@ -14,13 +14,87 @@ void Zadacha10 ()
             number = Convert.ToInt32(Console.ReadLine());
         }
     }
-
+// Поиск второй цифры в числе который ввел пользователь.
     int result = number % 100 /10;
     Console.WriteLine($"Второрая цифра этого числа:{result}");
-
 }
 
+// Задача 13: Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+void Zadacha13VarianString ()
+{
+    Console.WriteLine("Задача 13: Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number <= 99)
+    {
+       Console.WriteLine("третьей цифры нет");  
+    }
+    else
+    {
+        string str = number.ToString();
+        Console.WriteLine(str[2]);  
+    }
+}
+void Zadacha13Varian2 ()
+{
+    Console.WriteLine("Задача 13: Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number <= 99)
+    {
+       Console.WriteLine("третьей цифры нет");  
+    }
+    else
+    {
+        int numberLength = number;
+        int Length = 0;
+        // Узнаем длину числа
+        while(numberLength > 0)
+        {
+            numberLength/= 10;
+            Length++;
+        }
+
+        int j = 1;
+        for (int i = 0; i < Length - 3; i++)
+        {
+            j = j * 10;
+        }
+
+        int result = (number / j)%10;
+        Console.WriteLine($"Третья цифра заданного число: {result}");
+    }
+}
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+void Zadacha15 ()
+{
+    Console.WriteLine("Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+    Console.WriteLine("Введите день недели (1-7)");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    // Проверка пользователя, корректно он ввел данные или нет.
+    while(number >= 8 ||  number <= 0)
+    {
+        if (number >= 8 ||  number <= 0)
+        {
+            Console.WriteLine("Повторите попытку");
+            number = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    if(number >= 6)
+    {
+        Console.WriteLine($"{number} - выходной");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - рабочий день");
+    }
+}
 Console.Clear();
 
 //Zadacha10 ();
-
+//Zadacha13VarianString ();
+//Zadacha13Varian2 ();
+Zadacha15 ();
